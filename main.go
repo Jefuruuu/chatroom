7c09faa677cc65b2f99973766d0e941b12abec85
Jefuruuu <a402070809@gmail.com>
2023-05-30 16:38:59 +0800

    add version to router package

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"chatroom/handlers"
+	v1 "chatroom/handlers/v1"
 	"chatroom/services/storage/tokens"
 	"chatroom/services/storage/users"
 
@@ -11,17 +11,18 @@ import (
 func main() {
 	// middleware -> token validation helper
 	router := gin.Default()//.Group("/v1")
-	v1 := router.Group("/v1")
+	v1Router := router.Group("/v1")
 	
 	userRepo := users.NewLoginRepo()
 	tokenRepo := tokens.NewTokenRepo()
 
-	v1.POST("/register", handlers.RegisterHandler(&userRepo))
-	v1.POST("/login", handlers.LoginHandler(&userRepo, &tokenRepo))
 
-	authorized := v1.Group("/auth")
-	authorized.Use(handlers.Authenticate(&tokenRepo))
-	authorized.POST("/logout", handlers.LogoutHandler(&tokenRepo))
-	
+	v1Router.POST("/register", v1.RegisterHandler(&userRepo))
+	v1Router.POST("/login", v1.LoginHandler(&userRepo, &tokenRepo))
+
+	authorized := v1Router.Group("/auth")
+	authorized.Use(v1.Authenticate(&tokenRepo))
+	authorized.POST("/logout", v1.LogoutHandler(&tokenRepo))
+
 	router.Run()
 }
